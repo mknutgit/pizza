d0b2753a4ad085717b89b39875081de1d73ec58f
Matt <mknut001@gmail.com>
2016-01-29 11:25:44 -0800

    added first specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,32 @@
-function Order(firstName, pizza, price) {
+function Pizza(firstName,size,toppings) {
   this.firstName = firstName;
-  this.pizza = []
-  this.price = []
-}
-Order.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-
-function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = street;
-  this.city = city;
-  this.state = state;
+  this.toppings = toppings;
+  this.price = Order.price;
 }
 
-Address.prototype.fullAddress = function() {
+Pizza.prototype.price = function() {
+  if (size === "small")
+    return (8 + this.toppings.length);
+}
+
+Pizza.prototype.order = function() {
   return this.type + ": " + this.street + ", " + this.city + ", " + this.state;
 }
 
-function resetFields() {
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
-}
-
 $(document).ready(function() {
-  $("#add-address").click(function() {
-  $("#new-addresses").append('<div class="new-address" id="postAddress">' +
-                              '<div class="form-group">' +
-                              '<div class="form-group address-type" id="postAddress">' +
-                                '<label id="postAddress">What type of address is this?</label>' +
-                                '<select type="select" id="postAddress" class="new-type">' +
-                                '<option type="address-type" id="postAddress">Home</option>' +
-                                '<option type="address-type" id="postAddress">Work</option>' +
-                                '</select>' +
-                              '</div class="form-group" id="postAddress">' +
-                                '<label for="new-street" id="postAddress">Street</label>' +
-                                '<input type="text" class="form-control new-street" id="postAddress">' +
-                              '</div>' +
-                              '<div class="form-group" id="postAddress">' +
-                                '<label for="new-city" id="postAddress">City</label>' +
-                                '<input type="text" class="form-control new-city" id="postAddress">' +
-                              '</div>' +
-                              '<div class="form-group" id="postAddress">' +
-                                '<label for="new-state" id="postAddress">State</label>' +
-                                '<input type="text" class="form-control new-state" id="postAddress">' +
-                              '</div>' +
-                              '</div>');
-});
-
-  $("form#new-contact").submit(function(event) {
+$("form#pizzaorder").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedFirstName = $("input#orderName").text();
+    var inputtedSize = $(".size option:selected").text();
+    var toppings = $('#toppings :checked').each(function() {
+      toppings.push($(this).val());
+    }
+  }
+    var newPizza = new Pizza(inputtedFirstName, inputtedSize, toppings);
 
-    $(".new-address").each(function() {
-     var inputtedType = $(this).find(".new-type option:selected").text();
-     var inputtedStreet = $(this).find("input.new-street").val();
-     var inputtedCity = $(this).find("input.new-city").val();
-     var inputtedState = $(this).find("input.new-state").val();
-     var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState)
-     newContact.addresses.push(newAddress)
-   });
 
-      $("#postAddress").hide();
-  //  $('#postAddress').not(this).hide();
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-
-    $(".contact").last().click(function() {
-      $("#show-Order").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-
-    resetFields();
 
 
   });
--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,24 @@
-describe('Contact', function() {
-  it("creates a new contact with the given properties", function() {
-    var testContact = new Contact("Rita","Moreno");
-    expect(testContact.firstName).to.equal("Rita");
-    expect(testContact.lastName).to.equal("Moreno");
-    expect(testContact.addresses).to.eql([]);
-  });
-
-  it("adds the fullName method to all contacts", function() {
-    var testContact = new Contact("Sherlock","Holmes");
-    expect(testContact.fullName()).to.equal("Sherlock Holmes");
-  });
-
-  describe('Address', function() {
-  it("creates a new address with the given specifications", function() {
-    var testAddress = new Address("123 Main St", "Test City","Test State");
-    expect(testAddress.street).to.equal("123 Main St");
-    expect(testAddress.city).to.equal("Test City");
-    expect(testAddress.state).to.equal("Test State");
+describe('Order', function()  {
+  it("creates a new Order with the given properties", function() {
+    var testOrder = new Order("Bill","Small");
+    expect(testOrder.firstName).to.equal("Bill");
+    expect(testOrder.size).to.equal("Small")
+    expect(testOrder.toppings).to.eql([]);
+    expect(testOrder.price).to.eql([]);
   });
 });
-});
+//
+//   it("adds the fullName method to all contacts", function() {
+//     var testContact = new Contact("Sherlock","Holmes");
+//     expect(testContact.fullName()).to.equal("Sherlock Holmes");
+//   });
+//
+//   describe('Address', function() {
+//   it("creates a new address with the given specifications", function() {
+//     var testAddress = new Address("123 Main St", "Test City","Test State");
+//     expect(testAddress.street).to.equal("123 Main St");
+//     expect(testAddress.city).to.equal("Test City");
+//     expect(testAddress.state).to.equal("Test State");
+//   });
+// });
+// });
